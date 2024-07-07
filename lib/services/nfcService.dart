bcf4d6111f3950fca23517e83d0bae4c1792fb81
elsaramji <elsaramegymahmoud404@gmail.com>
2024-07-07 19:04:29 +0300

    read nfc2

--- lib/services/nfcService.dart
+++ lib/services/nfcService.dart
@@ -1,8 +1,6 @@
-// nfcService.dart
-
+// services/nfcService.dart
 
 import 'package:flutter_nfc_kit/flutter_nfc_kit.dart';
-
 
 class NfcService {
   static readNFC() async {
@@ -14,20 +12,6 @@ class NfcService {
           probeWebUSBMagic: true,
         );
         print(tag.toJson());
-      } catch (e) {
-        print(e.toString());
-      }
-    }
-  }
-
-  static writeNFC() async {
-    if (NFCAvailability.available == await FlutterNfcKit.nfcAvailability) {
-      try {
-         var response = await FlutterNfcKit.transceive(
-         "Hello Dart!",
-          timeout: const Duration(seconds: 5),
-        );
-        print(response);
       } catch (e) {
         print(e.toString());
       }
